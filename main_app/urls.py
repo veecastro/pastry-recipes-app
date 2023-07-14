c51c0024f5d9ecb95ea1561fe8f1fa0b52639da8
veecastro <vcastro.zrt@gmail.com>
2023-07-14 19:35:57 -0400

    edited RecipeCreate and added about

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('', views.home, name='home'),
 
    path('recipes/', views.recipes_index, name='index'),
-   path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
+   path('recipes/create/', views.RecipeCreate.as_view(), name='recipe_create'),
+   path('about/', views.about, name='about'),
 
  ]
