1d2d6cf5d95c814d7dda28ad86c64b4479d43941
noordibou <noor.dibou@hotmail.com>
2023-07-14 23:01:25 -0500

    working on viewing recipe error

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('recipes/create/', views.RecipeCreate.as_view(), name='recipe_create'),
     path('about/', views.about, name='about'),
     path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
-    path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipe_update'),
-    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipe_delete'),
+   
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 # from.django.views.generic import ListView
 from .models import Pastryrecipe
 
@@ -15,7 +15,7 @@ def recipes_index(request):
     'pastryrecipes': pastryrecipes
   })
 
-def recipe_detail(request, recipe_id):
+def recipes_detail(request, recipe_id):
  pastryrecipe = Pastryrecipe.objects.get(id=recipe_id)
  return render(request, 'recipes/detail.html', {
    'pastryrecipe': pastryrecipe
@@ -32,11 +32,3 @@ class RecipeCreate(CreateView):
   template_name = 'main_app/recipe_form.html'
   success_url = '/recipes' 
 
-class RecipeUpdate(UpdateView):
-  model = Pastryrecipe
-  fields = '__all__'
-  success_url = '/recipes'
-
-class RecipeDelete(DeleteView):
-  model = Pastryrecipe
-  success_url = '/recipes'
