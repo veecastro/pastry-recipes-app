15eef96e7a6d3d75d23641a7398dcbf551e3e15a
noordibou <noor.dibou@hotmail.com>
2023-07-17 00:52:05 -0500

    add admin auth & working on categories

--- main_app/models.py
+++ main_app/models.py
@@ -3,23 +3,23 @@ from django.urls import reverse
 
 # Create your models here.
 
-CATEGORY = (
-   ('CC', 'Cakes and Cupcakes'),
-   ('PT', 'Pies and Tarts'),
-   ('CB', 'Cookies and Bisquits'),
-   ('PP', 'Pastries and Puff Pastry'),
-   ('FD', 'Frozen Desserts'),
-   ('PC', 'Puddings and Custards'),
-   ('FR', 'Fruit-based Desserts'),
-   ('ID', 'International Desserts'),
-   ('OT', 'Other Desserts'),
-)
+# CATEGORY = (
+#    ('CC', 'Cakes and Cupcakes'),
+#    ('PT', 'Pies and Tarts'),
+#    ('CB', 'Cookies and Bisquits'),
+#    ('PP', 'Pastries and Puff Pastry'),
+#    ('FD', 'Frozen Desserts'),
+#    ('PC', 'Puddings and Custards'),
+#    ('FR', 'Fruit-based Desserts'),
+#    ('ID', 'International Desserts'),
+#    ('OT', 'Other Desserts'),
+# )
 
 class Pastryrecipe(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=4,
-      choices=CATEGORY,
-      default=CATEGORY[0][0])
+    # category = models.CharField(max_length=4,
+    #   choices=CATEGORY,
+    #   default=CATEGORY[0][0])
     preptime = models.IntegerField()
     cookingtime = models.IntegerField()
     totaltime = models.IntegerField()
@@ -42,13 +42,13 @@ class Photo(models.Model):
       return f"Photo for recipe_id: {self.recipe_id} @ {self.url}"
 
 
-class Category(models.Model):
-    name = models.CharField(
-      max_length=4,
-      choices=CATEGORY,
-      default=CATEGORY[0][0]
-    )
+# class Category(models.Model):
+#     name = models.CharField(
+#       max_length=4,
+#       choices=CATEGORY,
+#       default=CATEGORY[0][0]
+#     )
+#     recipe = models.ManyToManyField(Pastryrecipe)
 
-
-    def __str__(self):
-      return f'{self.get_category_display()}'
+#     def __str__(self):
+#       return f'{self.get_category_display()}'
