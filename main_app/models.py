ef49f48250d017aeb757152ce7384a412851b787
noordibou <noor.dibou@hotmail.com>
2023-07-17 00:30:52 -0500

    adding categories to model

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,30 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+CATEGORY = (
+   ('CC', 'Cakes and Cupcakes'),
+   ('PT', 'Pies and Tarts'),
+   ('CB', 'Cookies and Bisquits'),
+   ('PP', 'Pastries and Puff Pastry'),
+   ('FD', 'Frozen Desserts'),
+   ('PC', 'Puddings and Custards'),
+   ('FR', 'Fruit-based Desserts'),
+   ('ID', 'International Desserts'),
+   ('OT', 'Other Desserts'),
+)
+
 class Pastryrecipe(models.Model):
     title = models.CharField(max_length=100)
+    category = models.CharField(max_length=4,
+      choices=CATEGORY,
+      default=CATEGORY[0][0])
     preptime = models.IntegerField()
     cookingtime = models.IntegerField()
     totaltime = models.IntegerField()
     yields = models.IntegerField()
-    instructions = models.TextField()
     ingredients = models.TextField()
+    instructions = models.TextField()
     img = models.ImageField(upload_to='recipes/', blank=True)
     
     def __str__(self):
@@ -24,3 +40,15 @@ class Photo(models.Model):
 
     def __str__(self):
       return f"Photo for recipe_id: {self.recipe_id} @ {self.url}"
+
+
+class Category(models.Model):
+    name = models.CharField(
+      max_length=4,
+      choices=CATEGORY,
+      default=CATEGORY[0][0]
+    )
+
+
+    def __str__(self):
+      return f'{self.get_category_display()}'
