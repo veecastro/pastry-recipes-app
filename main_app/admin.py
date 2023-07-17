71e11ddbf003be089c7f513fe0c1ee08a2fa4edd
noordibou <noor.dibou@hotmail.com>
2023-07-17 18:55:59 -0500

    updating categories

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from main_app.models import Pastryrecipe, Photo
+from main_app.models import Pastryrecipe, Photo, Category
 # Register your models here.
 admin.site.register(Pastryrecipe)
 admin.site.register(Photo)
+admin.site.register(Category)
